283d8ea5bef32cef7fa41db5da3280bf9a5c0922
Rahul <Shejolerahul2@gmail.com>
2026-07-29 22:23:37 +0530

    Update user profile name to Sonia Gandhi

--- src/data/initialProfile.ts
+++ src/data/initialProfile.ts
@@ -1,25 +1,25 @@
 import { UserProfile } from '../types';
 
 export const initialProfile: UserProfile = {
-  name: "सौ. स्नेहा रमेश आंब्रे",
-  designation1: "नगरसेविका - ठा.म.पा. प्रभाग क्र. ४ (ब)",
-  designation2: "कार्याध्यक्षा - नवयुग मित्र मंडळ (रजि.)",
+  name: "Sonia Gandhi",
+  designation1: "Member of Parliament - Rae Bareli",
+  designation2: "Former President - Indian National Congress",
   contactNumber: "09768997272",
-  email: "sneharambre@gmail.com",
-  address: "नवयुग मित्र मंडळ कार्यालय, प्रभाग क्र. ४ (ब), ठाणे (पश्चिम)",
-  registrationNo: "ठा.म.पा. प्रभाग क्र. ४ (ब) / रजि. क्र. ४०८",
+  email: "soniagandhi@gmail.com",
+  address: "10 Janpath, New Delhi - 110011",
+  registrationNo: "INC / MP / Rae Bareli",
   avatarUrl: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=400&auto=format&fit=crop&q=80",
   partyLogoUrl: "",
-  partyName: "भारतीय जनता पार्टी (BJP)",
-  primaryBrandColor: "#FF7200", // Bright Saffron Orange
+  partyName: "Indian National Congress (INC)",
+  primaryBrandColor: "#FF7200",
   secondaryBrandColor: "#FF7200",
   preferredLanguage: "Marathi",
   socialLinks: {
-    instagram: "https://instagram.com/sneharambre",
-    twitter: "https://x.com/sneharambre",
+    instagram: "https://instagram.com/soniagandhi",
+    twitter: "https://x.com/soniagandhi",
     whatsapp: "https://wa.me/919768997272",
-    facebook: "https://facebook.com/sneharambre",
-    mapLocation: "https://maps.google.com/?q=Thane",
+    facebook: "https://facebook.com/soniagandhi",
+    mapLocation: "https://maps.google.com/?q=New+Delhi",
   },
 };
 
